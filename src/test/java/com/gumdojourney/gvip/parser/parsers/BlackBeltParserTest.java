df71c954cb04f93b29ec0271728c9553b3cdcc82
Michael Remijan <mjremijan@yahoo.com>
2026-07-24 10:29:26 -0500

    Try with 2nd dan 3rd form

--- src/test/java/com/gumdojourney/gvip/parser/parsers/BlackBeltParserTest.java
+++ src/test/java/com/gumdojourney/gvip/parser/parsers/BlackBeltParserTest.java
@@ -55,7 +55,7 @@ public class BlackBeltParserTest {
         assertEquals(18, md.getTags().size());
     }
 
-        @Test
+    @Test
     public void testParseBlackBelt3() {
         BlackBeltParser parser = new BlackBeltParser();
         String filename = "Black belt 1st Dan 1st self-defense form [유단자 1단 두번째 격검] - Haidong Gumdo (2026-04-05 14.15).mp4";
@@ -69,6 +69,24 @@ public class BlackBeltParserTest {
         assertEquals("2026-04-05", md.getRecordingDate().toString());
         assertEquals(1, md.getPlaylists().size());
         assertEquals("1st Dan 1st Self-Defense", md.getPlaylists().get(0));
+        assertEquals(false, md.isMadeForKids());
+        assertEquals(18, md.getTags().size());
+    }
+
+    @Test
+    public void testParseBlackBelt4() {
+        BlackBeltParser parser = new BlackBeltParser();
+        String filename = "Black belt 2nd Dan 3rd self-defense form [유단자 1단 두번째 격검] - Haidong Gumdo (2026-04-05 14.15).mp4";
+        Optional<Metadata> mdOpt = parser.parse(null, filename);
+        assertTrue(mdOpt.isPresent());
+
+        Metadata md = mdOpt.get();
+        assertEquals(filename, md.getSourceFilename());
+        assertEquals("2nd Dan 3rd Self-Defense (2026-04) - Haidong Gumdo", md.getTitle());
+        assertEquals("Black belt 2nd Dan 3rd self-defense form [유단자 1단 두번째 격검] - Haidong Gumdo (2026-04-05 14.15)", md.getDescription());
+        assertEquals("2026-04-05", md.getRecordingDate().toString());
+        assertEquals(1, md.getPlaylists().size());
+        assertEquals("2nd Dan 3rd Self-Defense", md.getPlaylists().get(0));
         assertEquals(false, md.isMadeForKids());
         assertEquals(18, md.getTags().size());
     }
